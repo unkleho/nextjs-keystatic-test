28ffae8b474d8ee5af39dd27648ac8a18358b7b8
Kaho Cheung <kaho@elefanttraks.com>
2024-02-10 11:14:55 +1100

    Logs

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -9,8 +9,17 @@ const reader = createReader(process.cwd(), keystaticConfig);
 export default async function Post({ params }: { params: { slug: string } }) {
   const post = await reader.collections.posts.read(params.slug);
   try {
-    const post2 = await reader.collections.posts.readOrThrow(params.slug);
-    console.log({ params, post, post2, posts: reader.collections.posts });
+    const allPosts = await reader.collections.posts.all();
+    const list = await reader.collections.posts.list();
+    const post2 = await reader.collections.posts.read(params.slug);
+    console.log({
+      params,
+      post,
+      post2,
+      posts: reader.collections.posts,
+      allPosts,
+      list,
+    });
   } catch (e) {
     console.log('readOrThrow', e);
   }
