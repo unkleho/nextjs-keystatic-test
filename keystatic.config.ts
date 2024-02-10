97ebe72736525014742482f96afe2bec1cc89c85
Kaho Cheung <kaho@elefanttraks.com>
2024-02-10 12:14:15 +1100

    Try public env

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,26 +1,26 @@
 import { config, fields, collection } from '@keystatic/core';
 
-const isProd = Boolean(process.env.VERCEL_ENV);
+const isProd = Boolean(process.env.NEXT_PUBLIC_VERCEL_ENV);
 
-console.log('keystatic.config', process.env.VERCEL_ENV);
+console.log('keystatic.config', process.env.NEXT_PUBLIC_VERCEL_ENV);
 
 export default config({
-  storage: {
-    kind: 'github',
-    repo: {
-      name: 'nextjs-keystatic-test',
-      owner: 'unkleho',
-    },
-  },
-  // storage: isProd
-  //   ? {
-  //       kind: 'github',
-  //       repo: {
-  //         name: 'nextjs-keystatic-test',
-  //         owner: 'unkleho',
-  //       },
-  //     }
-  //   : { kind: 'local' },
+  // storage: {
+  //   kind: 'github',
+  //   repo: {
+  //     name: 'nextjs-keystatic-test',
+  //     owner: 'unkleho',
+  //   },
+  // },
+  storage: isProd
+    ? {
+        kind: 'github',
+        repo: {
+          name: 'nextjs-keystatic-test',
+          owner: 'unkleho',
+        },
+      }
+    : { kind: 'local' },
   collections: {
     posts: collection({
       label: 'Posts',
