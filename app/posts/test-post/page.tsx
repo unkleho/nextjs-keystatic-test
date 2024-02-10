ed35159cbdaf9fdd47e60b280eb8351c5415e693
Kaho Cheung <kaho@elefanttraks.com>
2024-02-10 11:38:06 +1100

    Try test post

---
+++ app/posts/test-post/page.tsx
@@ -0,0 +1,44 @@
+import { DocumentRenderer } from '@keystatic/core/renderer';
+
+import { createReader } from '@keystatic/core/reader';
+import keystaticConfig from '../../../keystatic.config';
+import Link from 'next/link';
+
+const reader = createReader(process.cwd(), keystaticConfig);
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  const post = await reader.collections.posts.read('test');
+  try {
+    const allPosts = await reader.collections.posts.all();
+    const list = await reader.collections.posts.list();
+    const post2 = await reader.collections.posts.read(params.slug);
+    console.log({
+      params,
+      post,
+      post2,
+      posts: reader.collections.posts,
+      allPosts,
+      list,
+      cwd: process.cwd(),
+      keystaticConfig,
+    });
+  } catch (e) {
+    console.log('readOrThrow', e);
+  }
+
+  return (
+    <>
+      <h1>Test Post (not dynamic)</h1>
+
+      {post && (
+        <>
+          <h1>{post?.title}</h1>
+          <DocumentRenderer document={await post.content()} />
+        </>
+      )}
+
+      <br />
+      <Link href="/">Back home</Link>
+    </>
+  );
+}
